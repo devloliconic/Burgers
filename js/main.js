18e703ba1436b970c2b738c156380fbb306c7b77
devloliconic <kirillevsmirnov@gmail.com>
2022-03-15 11:39:53 +0700

    add js and style

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// scroll
+const links = document.querySelectorAll(".banner-link");
+
+for (const link of links) {
+link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+});
+}
+window.onscroll = function(){scrollFunction()};
+//buttonup
+const upbuttons = document.querySelectorAll(".button-up");
+
+for (const upbutton of upbuttons) {
+        upbutton.addEventListener("click", clickHandler);
+    }
+
+    function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+});
+}
+
+function scrollFunction(){
+    if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+        document.getElementById('btnUp').className = 'button-up visible';
+    } else {
+        document.getElementById('btnUp').className = 'button-up hidden';
+    }
+}
